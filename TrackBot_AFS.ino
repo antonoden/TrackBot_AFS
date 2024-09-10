9fdab5f03a86fc7a46f6f7bd8170d7c9ddfe278b
Anton Odén <antonoden@hotmail.com>
2024-09-10 16:45:06 +0200

    Taskhandling+STances
    Added taskhandling. Experimented alot. Added more stances. Code is not commented well.

--- TrackBot_AFS.ino
+++ TrackBot_AFS.ino
@@ -6,13 +6,14 @@
 void setup() {
   // put your setup code here, to run once:
   zInitialize();
+  zScheduleTask(getSensorStatus, 6, 2);
+  zScheduleTask(drive, 6, 2);
+  zScheduleTask(avoidManeuver, 20, 5);
+  zStart();
 }
 /*
 
 */
 void loop() {
   
-  mainloop();
-
-  zBlockingDelay(5);
 }
